81cd586cb33bb5e697d312de8f7a9c30ecc15799
Leilani Reich <reichld@hawaii.edu>
2022-11-08 21:02:12 -1000

    more work & experimenting w/ sliders

--- app.py
+++ app.py
@@ -5,19 +5,27 @@ from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.SUPERHERO])
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 
+# Reading in data / Preprocessing
 
+# Visualizing map of homeless shelters
 shelters_df = pd.read_csv('homeless_shelters.csv')
-#print(hotels_df.head)
 
-fig = px.scatter_mapbox(shelters_df, lat='latitude', lon='longitude', mapbox_style='carto-positron', width=800)
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig1 = px.scatter_mapbox(shelters_df, lat='latitude', lon='longitude', mapbox_style='open-street-map', width=800, zoom=5.8)
+fig1.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
+# Visualizing chronic homelessness
+chronic_homelessness_df = pd.read_csv('chronic_homelessness__2005_to_2019_-1.csv')
+chronic_homelessness_count_cols = ['Oahu', 'Hawaii, Maui, and Kauai', 'Total']
+
+
+# App
 app.title = 'Hawaii Homelessness Dashboard'
 
 app.layout = dbc.Container(children=[
@@ -35,11 +43,75 @@ app.layout = dbc.Container(children=[
 
             dbc.Col(
                 dcc.Graph(
-                    id='example-graph',
-                    figure=fig,
-                ),  width=8, style={"margin": "auto"}
+                    id='fig1',
+                    figure=fig1,
+                    style={"color": "white"}
+                ),  width=8, style={"margin": "auto", "height": "80%"}
         )
-    ])], className="dbc", fluid=True)
+    ]),
+    
+    dbc.Row([
+        dbc.Col(
+            width=4
+        ),
+        dbc.Col(
+            dcc.Graph(
+            id="fig2"
+            ), width=8, style={"padding-top": "50px", "height": "30%"}
+        )
+
+    ]),
+
+    dbc.Row([
+        dbc.Col(
+            width=4
+        ),
+        dbc.Col(
+        dcc.Slider(
+               min=chronic_homelessness_df['Year'].min(),
+               max=chronic_homelessness_df['Year'].max(),
+               step=None,
+               value=chronic_homelessness_df['Year'].min(),
+               marks={str(year): str(year) for year in chronic_homelessness_df['Year'].unique()},
+               id='slider-year',
+           ), width=8
+        )
+    ]),
+    
+    ], className="dbc", fluid=True)
+
+# def get_updated_chart(year):
+
+#     filtered_df = chronic_homelessness_df[chronic_homelessness_df['Year'] == year]
+#     print("filtered_df", filtered_df)
+#     print("Year", year)
+#     fig2 = px.line(filtered_df, x="Year", y=chronic_homelessness_count_cols, markers=True,
+#     color_discrete_map={"Total": "red", "Oahu": "orange", "Hawaii, Maui, and Kauai": "blue"}
+#     )
+
+#     fig2.update_layout(legend_title='', title="Chronic Homelessness Counts across Hawaiian Islands", 
+#     yaxis_title="Counts of Chronically Homeless", hovermode="x unified",
+#     paper_bgcolor="#4e5d6c", font=dict(color="white"))
+#     fig2.update_traces(hovertemplate='%{y}',  line=dict(width=3))
+#     return fig2
+
+
+@app.callback(
+Output('fig2', 'figure'),
+Input('slider-year', 'value'),)
+def update_graph(value):
+    filtered_df = chronic_homelessness_df[chronic_homelessness_df['Year'] <= value]
+
+    fig2 = px.line(filtered_df, x="Year", y=chronic_homelessness_count_cols, markers=True,
+    color_discrete_map={"Total": "red", "Oahu": "orange", "Hawaii, Maui, and Kauai": "blue"}
+    )
+
+    fig2.update_layout(legend_title='', title="Chronic Homelessness Counts across Hawaiian Islands", 
+    yaxis_title="Counts of Chronically Homeless", hovermode="x unified",
+    paper_bgcolor="#4e5d6c", font=dict(color="white"))
+    fig2.update_traces(hovertemplate='%{y}',  line=dict(width=3))
+    fig2.update_xaxes(tickmode="array", tickvals=list(chronic_homelessness_df['Year']))
+    return fig2
 
 #app.run_server(debug=True)
 if __name__ == '__main__':
